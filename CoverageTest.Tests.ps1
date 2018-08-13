6b929cebf7c57c897caa36dae0394f5961b1e1b2
psymonn <40018954+psymonn@users.noreply.github.com>
2018-08-13 21:27:08 +1000

    update

---
+++ CoverageTest.Tests.ps1
@@ -0,0 +1,24 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe 'Demonstrating Code Coverage' {
+    It 'Calls FunctionOne with no switch parameter set' {
+        FunctionOne | Should Be 'SwitchParam was not set'
+    }
+    
+    It 'Calls FunctionTwo' {
+        FunctionTwo | Should Be 'I get executed'
+    }
+
+     It 'Calls FunctionTwo2' {
+        FunctionTwo | Should Be 'I do not'
+    }
+}
+
+#Invoke-Pester .\CoverageTest.Tests.ps1 
+#Invoke-Pester .\CoverageTest.Tests.ps1 -CodeCoverage .\CoverageTest.ps1
+#Invoke-Pester .\CoverageTest.Tests.ps1 -CodeCoverage .\CoverageTest.ps1 -OutputFile TestResults.xml -OutputFormat NUnitXml
+#Invoke-Pester .\CoverageTest.Tests.ps1 -OutputFile TestResults2.xml -OutputFormat NUnitXml
+#Invoke-Pester .\CoverageTest.Tests.ps1 -CodeCoverage @{Path = '.\CoverageTest.ps1'; Function = 'FunctionOne' }
+#Invoke-Pester .\CoverageTest.Tests.ps1 -CodeCoverage @{Path = '.\CoverageTest.ps1'; StartLine = 7; EndLine = 14 }
---
+++ CoverageTest.ps1
@@ -0,0 +1,17 @@
+﻿function FunctionOne ([switch] $SwitchParam)
+{
+    if ($SwitchParam)
+    {
+        return 'SwitchParam was set'
+    }
+    else
+    {
+        return 'SwitchParam was not set'
+    }
+}
+
+function FunctionTwo
+{
+    return 'I get executed'
+    return 'I do not'
+}
