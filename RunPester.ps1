f580a6b513f1aae5717bae50f6c2936417f9ebf2
psymonn <40018954+psymonn@users.noreply.github.com>
2018-08-13 21:36:40 +1000

    update

--- RunPester.ps1
+++ RunPester.ps1
@@ -21,7 +21,7 @@ param (
     
         # Go to the Pester tests directory
  	cd "$source\Pester"
-		Invoke-Pester .\CoverageTest.Tests.ps1 -CodeCoverage .\CoverageTest.ps1 -verbose -OutputFile "$Source\Pester\$OutFile"  -OutputFormat NUnitXml
+		Invoke-Pester .\CoverageTest.Tests.ps1 -CodeCoverage .\CoverageTest.ps1 -verbose -OutputFile "$Source\Pester\test2.xml"  -OutputFormat NUnitXml
         # Execute tests
  	    Invoke-Pester -verbose -OutputFile "$Source\Pester\$OutFile"  -OutputFormat NUnitXml `
                       -EnableExit -Script @{ Path = "$Source\Pester"; Parameters = @{
